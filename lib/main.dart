d1b9a4bdb275152cdc3aac6b7b7bfec7b6ac07e4
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2024-12-18 21:42:15 +0100

    data structure in flutter

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+void main() {
+  //data type
+  //int
+  int age = 21;
+  //String
+  String name = 'Brown';
+  //bool
+  bool male = true;
+  //list of string
+  List<String> fruit = ["orange", 'pineapple', 'banana'];
+  //list of int
+  List<int> preferredNum = [1, 94, 28, 09];
+  //map of type string dynamic, we can have map of any couple of type like <int, string> etc
+  Map<String, dynamic> personalData= {'name':name, "age":age, "male":male, 'fruit':fruit, "preferredNum":preferredNum};
+  print(personalData);
+}
+
+

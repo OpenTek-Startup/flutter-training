08e98ffc7449e1b6504b0bdd996262fbd7271dfe
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2024-12-19 02:37:37 +0100

    list methods

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,27 @@ void main() {
   //map of type string dynamic, we can have map of any couple of type like <int, string> etc
   Map<String, dynamic> personalData= {'name':name, "age":age, "male":male, 'fruit':fruit, "preferredNum":preferredNum};
   print(personalData);
+
+  //list method
+  //adding one element at time in the list
+ fruit.add('mango');
+   print(personalData);
+
+ //adding multiple elements, we passed a list to the addAll() method.
+  fruit.addAll(['lemon', 'water melon']);
+    print(personalData);
+  //Sorting a table: fetching data in the ascending order
+  fruit.sort();
+    print(personalData);
+  fruit.clear();
+    print(personalData);
+  //contains fucntion, return a bool value if the table contains a value or not
+    fruit.addAll(['lemon', 'water melon']);
+  bool answer = fruit.contains('apple');
+    print(answer);
+  //the where() function return the index of a list element if exist
+  var result = fruit.where((element)=>element=='lemon');
+  print(result);
 }
 
 

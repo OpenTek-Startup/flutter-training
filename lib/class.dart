c7c6768c1c2427854ca2ac5eb3eb6de04e84cf2f
BROWN-PROGRAMTECH <aldenovpoutine99@gmail.com>
2024-12-19 19:59:18 +0100

    Class management

---
+++ lib/class.dart
@@ -0,0 +1,13 @@
+class Person{
+  String displayUserName(String name){
+    return name;
+  }
+
+  int displayAge(int age){
+    return age;
+  }
+
+  double displayGrade(double grade){
+    return grade;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // import 'package:training/ListMethod.dart';
 // import 'package:training/MapMethod.dart';
 // import 'package:training/dataType.dart';
+import 'package:training/class.dart';
 import 'package:training/functionCall.dart';
 import 'package:training/functionInsideClass.dart';
 import 'package:training/typeOfFunctionParameter.dart';
@@ -24,6 +25,17 @@ positionalParameter(12, "name", [1, 'date', {'name':'Brown', 'age':21}]);
 
 //calling the name parameter. Here the order is not counting here
 nameParameter(password: 'password', name: 'Cedric', email: 'email@gmail.com', );
+
+Person person = new Person();
+int age = 21;
+String name = "Brown Alden";
+double grade = 3.4;
+int resultAge = person.displayAge(age);
+print(resultAge);
+double resultGrade = person.displayGrade(grade);
+print(resultGrade);
+String resultName = person.displayUserName(name);
+print(resultName);
 }
 
 
